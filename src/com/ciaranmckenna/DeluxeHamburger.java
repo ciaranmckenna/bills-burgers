e2deea99890464e3ba57ffc8413f12a571f92366
ciaranmckenna <hello@ciaranmckenna.com>
2020-07-02 09:13:48 +0100

    Adding the classes, basic methods and fields to support the project

---
+++ src/com/ciaranmckenna/DeluxeHamburger.java
@@ -0,0 +1,8 @@
+package com.ciaranmckenna;
+
+public class DeluxeHamburger extends Hamburger {
+
+    public DeluxeHamburger(String meat, String breadRoll, double price) {
+        super(meat, breadRoll, price);
+    }
+}
---
+++ src/com/ciaranmckenna/HealthyBurger.java
@@ -0,0 +1,8 @@
+package com.ciaranmckenna;
+
+public class HealthyBurger extends Hamburger {
+
+    public HealthyBurger(String meat, String breadRoll, double price) {
+        super(meat, breadRoll, price);
+    }
+}
